575fd2db8965d7109343830835c8753040d81c19
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-02-05 12:47:26 +0530

    starting js

---
+++ script.js
@@ -0,0 +1,19 @@
+let questions=[
+    {
+        question:"this is question 1",
+        option1:"option 1",
+        option2:"option 2",
+        option3:"option 3",
+        option4:"option 4",
+    },
+    {
+        question:"this is question 2",
+        option1:"option 1",
+        option2:"option 2",
+        option3:"option 3",
+        option4:"option 4",
+    }
+]
+
+// console.log(questions[0]);
+console.log(questions[0].question);
